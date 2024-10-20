96ef230d7dd55a12a215276a011ce47a3e671f2a
Leonardo Silva <126107687+leo-vasi@users.noreply.github.com>
2024-10-20 20:06:31 -0300

    Usando StringBuilder

---
+++ string-builder-test/src/main/java/com/leo/application/App.java
@@ -0,0 +1,35 @@
+package com.leo.application;
+
+import com.leo.entities.Comment;
+import com.leo.entities.Post;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws ParseException {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+
+        Comment c1 = new Comment("Have a nice trip!");
+        Comment c2 = new Comment("Wow, that's awesome!");
+
+        Post p1 = new Post(sdf.parse("21/06/2018 13:05:44"), "Traveling to New Zealand", "I'm going to visit this wonderfull country!", 12);
+        p1.addComment(c1);
+        p1.addComment(c2);
+
+        Comment c3 = new Comment("Good night");
+        Comment c4 = new Comment("May the force be with you");
+        Post p2 = new Post(sdf.parse("28/07/2018 23:14:19"), "Good night guys", "See you tomorrow", 5);
+
+        p2.addComment(c3);
+        p2.addComment(c4);
+
+        System.out.println(p1);
+        System.out.println(p2);
+        sc.close();
+    }
+}
