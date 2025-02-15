95ea8da1f5c498a258c61829a6e6cdd51a691e95
Leonardo Silva <126107687+leo-vasi@users.noreply.github.com>
2025-02-15 17:58:05 -0300

    Loop com acúmulo

---
+++ desafioTeste5/src/main/java/org/example/Main.java
@@ -0,0 +1,47 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int totalSaquesTentados = 0;
+        int totalSaquesCertos = 0;
+        int totalBloqueiosTentados = 0;
+        int totalBloqueiosCertos = 0;
+        int totalAtaquesTentados = 0;
+        int totalAtaquesCertos = 0;
+
+        sc.nextLine();
+
+        for (int i = 0; i < n; i++) {
+            String nome = sc.nextLine();
+            int s = sc.nextInt();
+            int b = sc.nextInt();
+            int a = sc.nextInt();
+            int s1 = sc.nextInt();
+            int b1 = sc.nextInt();
+            int a1 = sc.nextInt();
+            sc.nextLine();
+
+            totalSaquesTentados += s;
+            totalSaquesCertos += s1;
+            totalBloqueiosTentados += b;
+            totalBloqueiosCertos += b1;
+            totalAtaquesTentados += a;
+            totalAtaquesCertos += a1;
+        }
+
+        double saquePercentual = (double) totalSaquesCertos / totalSaquesTentados * 100;
+        double bloqueioPercentual = (double) totalBloqueiosCertos / totalBloqueiosTentados * 100;
+        double ataquePercentual = (double) totalAtaquesCertos / totalAtaquesTentados * 100;
+
+        System.out.println("Pontos de Saque: %" +saquePercentual);
+        System.out.println("Pontos de Bloqueio: %" +bloqueioPercentual);
+        System.out.println("Pontos de Ataque: %"+ ataquePercentual);
+
+        sc.close();
+    }
+}
